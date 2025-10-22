419066e9e6707cb10832a77638f43c6b15a15edd
Master2DS <dslee111@gmail.com>
2025-10-22 14:31:41 +0900

    다이어리 update

--- travel/urls.py
+++ travel/urls.py
@@ -5,4 +5,13 @@ from travel import views
 app_name = "travel"
 urlpatterns = [
     path("list/", views.travel_list, name="travel_list"),
+    path("upload/", views.upload_diary_entry, name="upload_diary_entry"),
+    path("upload/<int:travel_id>/", views.upload_diary_entry, name="upload_diary_entry_to_travel"),
+    path("diary/", views.diary_list, name="diary_list"),
+    path("diary_home/", views.diary_home, name="diary_home"),
+    path("create_diary/", views.create_travel_diary, name="create_travel_diary"),
+    path("diary/<int:pk>/", views.travel_diary_detail, name="travel_diary_detail"),
+    path("diary/<int:pk>/edit/", views.edit_travel_diary, name="edit_travel_diary"),
+    path("diary_entry/<int:pk>/edit/", views.edit_diary_entry, name="edit_diary_entry"),
+    path("diary_entry/<int:pk>/delete/", views.delete_diary_entry, name="delete_diary_entry"),
 ]
